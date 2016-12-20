1eec328dee8525e44ff2d3b94283135f22b0859b
mohit <mohit.gupta@clever.com>
2016-12-20 01:37:39 +0000

    use $npm_token

--- test_drone.sh
+++ test_drone.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -e -u
 
-sed -i.bak s/\${npm_auth_token}/$npm_auth_token/ .npmrc_docker
+sed -i.bak s/\${npm_auth_token}/$npm_token/ .npmrc_docker
 mv .npmrc_docker .npmrc
 npm install
 
