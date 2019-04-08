be8cd3b7be68fa7e5b4edbbe600d76be492ba5bf
Arsalan <arsalan.sufi@clever.com>
2019-04-08 18:46:32 +0000

    Set up new file-per-collection Redux store structure

--- src/ui/store/index.ts
+++ src/ui/store/index.ts
@@ -3,10 +3,21 @@ import { applyMiddleware, combineReducers, compose, createStore } from "redux";
 import { devToolsEnhancer } from "redux-devtools-extension/developmentOnly";
 import thunkMiddleware from "redux-thunk";
 
-import * as reducers from "./reducers";
+import * as counter from "./counter";
 
-const appReducer = enableBatching(combineReducers({ ...reducers }));
+const appReducer = enableBatching(combineReducers({
+  [counter.storeKey]: counter.reducer,
+}));
+
 export const store = createStore(appReducer, compose(
   applyMiddleware(thunkMiddleware),
   devToolsEnhancer({}),
 ));
+
+export const selectors = {
+  ...counter.selectors,
+};
+
+export const actions = {
+  ...counter.actions,
+};
--- src/ui/store/reducers/counter.ts
+++
@@ -1,9 +0,0 @@
-import { newAsyncDataReducer } from "clever-frontend-utils";
-import { handleActions } from "redux-actions";
-import * as actions from "../actions";
-
-export const incrementCounter = handleActions({
-  [actions.incrementCounter]: newAsyncDataReducer(
-    (state, action) => action.payload,
-  ),
-}, { data: { value: 0 }});
