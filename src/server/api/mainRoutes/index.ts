fdda79401d3cf66873b8387e4c1d59af00ec5f16
Arsalan <arsalan.sufi@clever.com>
2019-04-10 22:28:40 +0000

    Remove old sample endpoints

--- src/server/api/mainRoutes/index.ts
+++
@@ -1,7 +0,0 @@
-export class MainRoutes {
-  fetchCleverWebsite = async (req, res) => {
-    const resp = await fetch("https://clever.com");
-    const text = await resp.text();
-    return res.json(200, text);
-  }
-}
