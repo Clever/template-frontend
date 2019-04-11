9e6ebf3e73f59c13b3a433d329e46078456f2f18
Arsalan <arsalan.sufi@clever.com>
2019-04-11 00:24:43 +0000

    Tie everything together

--- src/server/app/index.ts
+++ src/server/app/index.ts
@@ -1,41 +1,48 @@
-import * as express from "express";
-import * as cookieParser from "cookie-parser";
+import { HttpMethod, patchExpressForPromises } from "clever-frontend-utils";
 import * as bodyParser from "body-parser";
+import * as cookieParser from "cookie-parser";
+import * as express from "express";
 import * as path from "path";
-import * as kayvee from "kayvee";
-import * as discovery from "clever-discovery";
-import { MainRoutes } from "../api/mainRoutes";
+
+import { Clients } from "src/server/modules";
+import { errorHandler } from "./errors/errorHandler";
+import { installApiEndpoints } from "src/server/api";
+import { installAuthEndpoints } from "src/server/auth";
+import { LandingPageEndpoint } from "src/server/pages";
+
 
 export function createApp() {
   const app = express();
+  patchExpressForPromises(app);
+
   app.use(cookieParser());
   app.use(bodyParser.json());
   app.use(bodyParser.urlencoded({ extended: true }));
 
   app.use(express.static(path.join(__dirname, "..", "..", "..", "__build")));
 
-  // Etags aren"t properly handled by all browsers so we outright disable all caching on
+  // Etags aren't properly handled by all browsers so we outright disable all caching on
   // our API methods.
   app.use((req, res, next) => {
     res.header("Cache-Control", "no-cache");
     next();
   });
 
+  app.set("views", `${__dirname}/src/server/pages/views`);
+
   app.get("/_healthcheck", (req, res) => {
     res.sendStatus(200);
   });
 
-  // TODO: add endpoints here!
+  Clients.initialize();
 
-  // TODO: remove this endpoint and its associated test.
-  // This endpoint is only here to demonstrate testing in
-  // index.test.ts.
-  app.get("/api/fetch_clever_website", new MainRoutes().fetchCleverWebsite);
+  installAuthEndpoints(app);
+  installApiEndpoints(app);
 
-  // catch-all route to serve the UI, if no API endpoints above match.
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "..", "..", "ui", "index.html"));
-  });
+  // Catch-all route to serve the UI, if no auth or API endpoints above match
+  (new LandingPageEndpoint()).install(app, HttpMethod.GET, "*");
+
+  app.use(errorHandler);
 
   return app;
 }
