b8e5299a00099330a7d4003635c815e87c9989ca
Arsalan <arsalan.sufi@clever.com>
2019-04-11 22:59:58 +0000

    Add comment noting 1-landing-page-endpoint common case

--- src/server/pages/LandingPageEndpoint.ts
+++ src/server/pages/LandingPageEndpoint.ts
@@ -7,6 +7,8 @@ import {
   userLoggedInMiddleware,
 } from "src/server/middleware";
 
+// For most repos, you should only need this one landing-page endpoint. Only if you need to serve
+// separate single-page apps for different user types, for example, should you need more than one
 export class LandingPageEndpoint extends Endpoint {
   constructor() {
     super();
