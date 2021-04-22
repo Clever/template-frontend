c4308af131bdfb0867a9e093f38e9ec17812347a
Rafael Garcia <rafael.garcia@clever.com>
2021-04-22 20:42:45 +0000

    use opentelemetry for tracing

--- scripts/startServer.sh
+++ scripts/startServer.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-./node_modules/.bin/ts-node --require tsconfig-paths/register --transpile-only ./src/server/index.ts
+./node_modules/.bin/ts-node --require ./tracing.ts --require tsconfig-paths/register --transpile-only ./src/server/index.ts
